a574c912b57515e0b4eae0a7b559f1bc5836035e
scottsc0de <scott5902@gmail.com>
2022-12-05 12:02:50 -0500

    working schema file

--- schema.sql
+++ schema.sql
@@ -3,8 +3,22 @@ CREATE DATABASE employees_db;
 
 USE employees_db;
 
-CREATE TABLE departments
+CREATE TABLE department (
+    id INT NOT NULL PRIMARY KEY,
+    name VARCHAR(30) -- to hold dept name
+);
 
-CREATE TABLE roles
+CREATE TABLE roles (
+    id INT NOT NULL,
+    title VARCHAR(30), -- to hold role title
+    salary DECIMAL, -- to hold role salary
+    department_id INT -- to hold reference to dept role belongs to
+);
 
-CREATE TABLE employees
+CREATE TABLE employee (
+    id INT PRIMARY KEY, 
+    first_name VARCHAR(30), -- to hold employee first name
+    last_name VARCHAR(30), -- to hold last name
+    role_id INT, -- to hold ref to employee rolde
+    manager_id INT -- to hold reference to another employee that is manager of the current employee (null if emp has no manager)
+);
